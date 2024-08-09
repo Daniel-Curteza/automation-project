d16b781c1c78a61178171dee50236d34cc453342
Daniel_C <daniel.curteza@gmail.com>
2024-08-09 23:25:28 +0300

    Created Calculator class

---
+++ src/main/java/homework/exercises/Calculator.java
@@ -0,0 +1,20 @@
+package homework.exercises;
+
+public class Calculator {
+    public static void main(String[] args){
+        if (args.length == 3){
+            float a = Float.parseFloat(args[0]);
+            float b = Float.parseFloat(args[2]);
+            String operator = args[1];
+            float result = 0;
+            switch (operator) {
+                case "+" -> result = a + b;
+                case "-" -> result = a - b;
+                case "*" -> result = a * b;
+                case "/" -> result = a / b;
+            }
+            System.out.println("Result is " + result);
+        } else System.err.println("3 args are needed!");
+
+    }
+}
